703b6f4f77a7efd806b093a155db9a4d98aea609
piebaroni <pietroba99@gmail.com>
2021-06-10 16:25:02 +0200

    springBoot base

---
+++ src/main/java/it/uniroma3/siw/explosionBox/ExplosionBoxApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.explosionBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplosionBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplosionBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/explosionBox/ExplosionBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.explosionBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplosionBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
