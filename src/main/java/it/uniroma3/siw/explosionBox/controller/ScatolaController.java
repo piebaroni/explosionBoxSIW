8a110ba808c7127c2878a1a2b9883491240f848a
piebaroni <pietroba99@gmail.com>
2021-06-19 09:24:25 +0200

    roba esame

--- src/main/java/it/uniroma3/siw/explosionBox/controller/ScatolaController.java
+++ src/main/java/it/uniroma3/siw/explosionBox/controller/ScatolaController.java
@@ -1,6 +1,8 @@
 package it.uniroma3.siw.explosionBox.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,12 +10,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.explosionBox.model.Credentials;
 import it.uniroma3.siw.explosionBox.model.Scatola;
+import it.uniroma3.siw.explosionBox.model.Utente;
+import it.uniroma3.siw.explosionBox.service.CredentialsService;
 import it.uniroma3.siw.explosionBox.service.DipendenteService;
 import it.uniroma3.siw.explosionBox.service.ScatolaService;
 
 @Controller
 public class ScatolaController {
+
+	@Autowired
+	private CredentialsService credentialsService;
 
 	@Autowired
 	private ScatolaService service;
@@ -29,9 +38,15 @@ public class ScatolaController {
 	@RequestMapping(value = "/scatola/{id}", method = RequestMethod.GET)
 	public String getScatola(@PathVariable("id") Long id, Model model) {
 		Scatola s = this.service.trovaPerId(id);
-		model.addAttribute("scatola", s);
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Credentials credentials = credentialsService.getCredentialsByUsername(userDetails.getUsername());
+		Utente u = credentials.getUtente();
+		if(s.getOrdine().getCompratore().equals(u)) {
+			model.addAttribute("scatola", s);
 		model.addAttribute("dipendente", s.getDipendente());
 		return "scatola.html";
+		}
+		return "index.html";
 	}
 
 	@RequestMapping(value = "/shopMini", method = RequestMethod.GET)
